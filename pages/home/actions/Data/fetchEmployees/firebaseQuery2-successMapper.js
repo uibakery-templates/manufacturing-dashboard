146246a0088525e7394ec61af8fd6f4401c9fa79
UI Bakery <template.maker@uibakery.io>
2024-07-16 12:17:50 +0000

    fix(datasource): fix ds and remove unnecessary actions

--- pages/home/actions/Data/fetchEmployees/firebaseQuery2-successMapper.js
+++
@@ -1,4 +0,0 @@
-return Object.entries({{data}}).map(([userId, userData]) => {
-  userData.id = userId;
-  return userData;
-});
--- pages/home/actions/Data/fetchUsersTasks/code.js
+++
@@ -1,4 +0,0 @@
-const employeeId = {{ui.productsTable.selectedRow.data.id}};
-let tasks = {{state.allTasks}}.filter((task) => task.assigned_employees.some((employee) => employee.employee_id === employeeId));
-
-return tasks;
